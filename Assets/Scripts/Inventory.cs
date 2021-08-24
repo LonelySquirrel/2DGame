19940a2dc9202351b4b7a3ce271de5c5c587df19
LonelySquirrel <79454326+LonelySquirrel@users.noreply.github.com>
2021-08-24 11:52:16 +0200

    Invetory(WIP), items and pickups

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    GameObject inventory;
+
+    //De är public så att andra scripts har tillgång till dem.
+    public bool[] slotsUsed; // ( Kollar om sloten är full.
+    public GameObject[] slots; 
+  
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if ( inventory == false)// Byter mellan att ha menyn öppen och stängd
+            {
+                print("Opening"); //debug
+
+                inventory.SetActive(true);
+            }
+            else if ( inventory == true)
+            {
+                print("Closing"); //debug
+
+                inventory.SetActive(false);
+
+            }
+        }
+        
+        
+    }
+
+   
+}
+
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Item : MonoBehaviour
+{
+    Inventory inventory;
+    [SerializeField]
+    string itemname;
+    [SerializeField]
+    Sprite itemSprite;
+    [SerializeField]
+    GameObject UseButton;
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Inventory").GetComponent<Inventory>(); //Hittar tar "Inventory" komponenten från spelaren
+    }
+
+    private void OnMouseDown()
+    {
+
+    }
+
+    public void Interact()
+    {
+        print("fucky you");
+        for (int i = 0; i < inventory.slots.Length; i++) //Kollar alla slots för en tom ruta
+        {
+            if (inventory.slotsUsed[i] == false)
+            {
+                inventory.slotsUsed[i] = true; // Gör att rutan är full
+                inventory.slots[i].name = itemname; // Ger namnet till objektet
+                inventory.slots[i].GetComponent<Image>().sprite = itemSprite; // Ger Spriten till objektet
+                inventory.slots[i].GetComponent<Image>().enabled = true; // Visar Spriten -Christian
+                Destroy(gameObject);
+                break;
+
+            }
+
+        }
+    }
+
+
+}
