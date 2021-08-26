b2c0feedae3d44a36a24104214c9ab345bd2566e
TCG-MrFrog <linus.barger44@gmail.com>
2021-08-26 19:50:22 +0200

    Started my Guard stuff
    I made my own scene as wel as made the start of the script and put it in my own folder in the script folder to make it clearer whos scripts we use so we don't fuck em up

---
+++ Assets/Scripts/Linus/guardAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guardAI : MonoBehaviour
+{
+
+    public List<Transform> wayPoints = new List<Transform>();
+    private Transform targetWayPoint;
+    private int targetWayPointIndex;
+    private float minDistance = 0.1f;
+    private int lastWayPointIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
